8b3445b81d2c95ae3ec094976544a318f02887cc
sbmshukla <sbmshukla17@gmail.com>
2025-09-03 13:51:06 +0530

    Access Removed From AWS

--- app.py
+++ app.py
@@ -100,4 +100,4 @@ async def predict_route(request: Request, file: UploadFile = File(...)):
 
 # Run the app
 if __name__ == "__main__":
-    app_run(app, host="0.0.0.0", port=8000)
+    app_run(app, host="localhost", port=8080)
